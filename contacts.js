1addc88a9e2d8e418da65b4a77678652bcd6a222
Vadx <vadim.lysenko@solidopinion.com>
2021-01-20 12:03:48 +0200

    Test 2

--- contacts.js
+++
@@ -1,9 +0,0 @@
-import { Container, Typography } from '@material-ui/core'
-
-const ContactsPage = () => (
-  <Container>
-    <Typography variant='h4' component='h1' gutterBottom>Hello, I'm the Contact page</Typography>
-  </Container>
-)
-
-export default ContactsPage
